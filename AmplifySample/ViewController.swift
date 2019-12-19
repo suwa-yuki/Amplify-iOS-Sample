a05b957c8aa656bb97fbd9b2683ab1e6651ead74
Yuki Suwa <suwa.yuki@classmethod.jp>
2019-12-19 12:14:14 +0900

    Initial Commit

---
+++ AmplifySample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmplifySample
+//
+//  Created by Yuki Suwa on 2019/12/19.
+//  Copyright © 2019 Yuki Suwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
